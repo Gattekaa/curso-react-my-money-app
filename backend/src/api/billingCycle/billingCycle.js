aeadcb44792bdc1fcd1c20c45e926ce74150cd00
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-10-31 12:18:55 -0300

    Finished Dashboard component WITHOUT Redux

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -1,4 +1,3 @@
-const { Schema } = require('mongoose')
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 
@@ -9,9 +8,9 @@ const creditSchema = new mongoose.Schema({
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: [true, "Informe o valor do débito!"] },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true,
-    enum: ['PAGO', 'PENDENTE', 'AGENDADO']}
+        enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
 const billingCycleSchema = new mongoose.Schema({
@@ -19,7 +18,7 @@ const billingCycleSchema = new mongoose.Schema({
     month: { type: Number, min: 1, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
     credits: [creditSchema],
-    debits: [debtSchema]
+    debts: [debtSchema]
 })
 
 module.exports = restful.model('BillingCycle', billingCycleSchema)
