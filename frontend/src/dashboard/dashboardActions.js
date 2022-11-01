d6819e2a17dec3a8f510e1380babe61d19467400
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-11-01 10:27:39 -0300

    Create getList action and your reducer

--- frontend/src/dashboard/dashboardActions.js
+++ frontend/src/dashboard/dashboardActions.js
@@ -1,7 +1,3 @@
-
-import axios from 'axios'
-const BASE_URL = 'http://localhost:3003/api'
-
 export function getSummary() {
     const request = axios.get(`${BASE_URL}/billingCycles/summary`)
     return {
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
+import BillingCyclesReducer from '../billingCycles/billingCyclesReducer'
 
 
 const rootReducer = combineReducers({
     dashboard: DashboardReducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCyclesReducer
 })
 
 export default rootReducer
