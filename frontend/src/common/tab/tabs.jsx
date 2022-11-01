e01f0de7a84d83d769900d58daf9be030e30d29e
Vinicius Gabriel <vinihbieel49@gmail.com>
2022-11-01 09:57:53 -0300

    Finished all Tab Components

---
+++ frontend/src/common/tab/tabs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Tabs = props => (
+     <div className="nav-tabs-custom">
+        {props.children}
+     </div>
+)
+
+
+export default Tabs
--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className="main-footer">
         <strong>
             Copyright &copy; 2022
-            <a href="#" target='_blank'>Clear</a>
+            <a href="#" target='_blank'> Clear</a>
         </strong>
     </footer>
 )
